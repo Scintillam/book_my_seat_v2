a9f11f0f35ae07b64f5e350f5896aafd3857fd11
Pablo Aldana <thorinroble@gmail.com>
2024-07-04 13:13:55 +0200

    improving performance by using keys and repaint

--- lib/src/widgets/seat_layout_widget.dart
+++ lib/src/widgets/seat_layout_widget.dart
@@ -23,8 +23,9 @@ class SeatLayoutWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print("BUILDING LAYOUT WIDGET");
     return Expanded(
-      child: SingleChildScrollView(
+      child:  SingleChildScrollView(
         primary: true,
         scrollDirection: Axis.vertical,
         child: Row(
@@ -32,23 +33,21 @@ class SeatLayoutWidget extends StatelessWidget {
             Padding(
               padding: EdgeInsets.only(top: scenarioHeight),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: List<int>.generate(
-                  stateModel.rows, (rowI) => rowI + 1).map<Widget>
-                (
-                  (index) => 
-                    Container(
-                      alignment: Alignment.center,
-                      padding: const EdgeInsets.only(right: 10),
-                      height: stateModel.seatSvgSize.toDouble(),
-                      child: Text(
-                        (index).toString(),
-                        textAlign: TextAlign.left,
-                        style: stageStyle ?? TextStyle(fontFamily: 'monospace'),
-                      ),
-                    )
-                ).toList()
-              ),
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children:
+                      List<int>.generate(stateModel.rows, (rowI) => rowI + 1)
+                          .map<Widget>((index) => Container(
+                                alignment: Alignment.center,
+                                padding: const EdgeInsets.only(right: 10),
+                                height: stateModel.seatSvgSize.toDouble(),
+                                child: Text(
+                                  (index).toString(),
+                                  textAlign: TextAlign.left,
+                                  style: stageStyle ??
+                                      TextStyle(fontFamily: 'monospace'),
+                                ),
+                              ))
+                          .toList()),
             ),
             Expanded(
               child: SingleChildScrollView(
@@ -65,38 +64,46 @@ class SeatLayoutWidget extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Container(
-                          width: MediaQuery.of(context).size.width * 0.8,
-                          height: scenarioHeight,
-                          color: Color(0xffeb3d8c),
-                          child: Center(child: Text('Escenario', style: stageStyle ?? TextStyle(fontFamily: 'monospace'))),
-                        ),
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        height: scenarioHeight,
+                        color: Color(0xffeb3d8c),
+                        child: Center(
+                            child: Text('Escenario',
+                                style: stageStyle ??
+                                    TextStyle(fontFamily: 'monospace'))),
+                      ),
                       ...List<int>.generate(stateModel.rows, (rowI) => rowI)
                           .map<Row>(
-                            (rowI) => Row(
-                              children: [
-                                ...List<int>.generate(stateModel.cols, (colI) => colI)
-                                    .map<Widget>(
-                                      (colI) => (colI > stateModel.currentSeats[rowI].length-1) ? 
-                                      SizedBox(width: stateModel.seatSvgSize.toDouble(), height: stateModel.seatSvgSize.toDouble()) 
-                                      : SeatWidget(
-                                          textStyle: numberStyle,
-                                          model: SeatModel(
-                                            seat: stateModel.currentSeats[rowI][colI],
-                                            rowI: rowI,
-                                            colI: colI,
-                                            seatSvgSize: stateModel.seatSvgSize,
-                                            pathSelectedSeat: stateModel.pathSelectedSeat,
-                                            pathDisabledSeat: stateModel.pathDisabledSeat,
-                                            pathSoldSeat: stateModel.pathSoldSeat,
-                                            pathUnSelectedSeat: stateModel.pathUnSelectedSeat,
-                                            pathOnHoldSeat: stateModel.pathOnHoldSeat,
-                                            pathOnBoughtSeat: stateModel.pathOnBoughtSeat
-                                          ),
-                                          onSeatStateChanged: onSeatStateChanged
-                                        ))
-                              ],
-                            ),
-                          )
+                        (rowI) => Row(
+                          children: [
+                            ...List<int>.generate(stateModel.cols,
+                                (colI) => colI).map<Widget>((colI) => (colI >
+                                    stateModel.currentSeats[rowI].length - 1)
+                                ? SizedBox(
+                                    width: stateModel.seatSvgSize.toDouble(),
+                                    height: stateModel.seatSvgSize.toDouble())
+                                : SeatWidget(
+                                    key: ValueKey('seat_${rowI}_$colI'),
+                                    textStyle: numberStyle,
+                                    model: SeatModel(
+                                        seat: stateModel.currentSeats[rowI]
+                                            [colI],
+                                        rowI: rowI,
+                                        colI: colI,
+                                        seatSvgSize: stateModel.seatSvgSize,
+                                        pathSelectedSeat:
+                                            stateModel.pathSelectedSeat,
+                                        pathDisabledSeat:
+                                            stateModel.pathDisabledSeat,
+                                        pathSoldSeat: stateModel.pathSoldSeat,
+                                        pathUnSelectedSeat:
+                                            stateModel.pathUnSelectedSeat,
+                                        pathOnHoldSeat: stateModel.pathOnHoldSeat,
+                                        pathOnBoughtSeat: stateModel.pathOnBoughtSeat),
+                                    onSeatStateChanged: onSeatStateChanged))
+                          ],
+                        ),
+                      )
                     ],
                   ),
                 ),
--- lib/src/widgets/seat_widget.dart
+++ lib/src/widgets/seat_widget.dart
@@ -5,8 +5,8 @@ import 'package:vive_models/vive_models.dart';
 
 class SeatWidget extends StatefulWidget {
   final SeatModel model;
-  final Future<void> Function(SeatAvailability currentSeat, TapUpDetails details)
-      onSeatStateChanged;
+  final Future<void> Function(
+      SeatAvailability currentSeat, TapUpDetails details) onSeatStateChanged;
   final TextStyle? textStyle;
 
   SeatWidget({
@@ -21,7 +21,6 @@ class SeatWidget extends StatefulWidget {
 }
 
 class _SeatWidgetState extends State<SeatWidget> {
-
   @override
   void initState() {
     super.initState();
@@ -29,38 +28,35 @@ class _SeatWidgetState extends State<SeatWidget> {
 
   @override
   Widget build(BuildContext context) {
-      return (widget.model.seat.status == SeatState.available || widget.model.seat.status == SeatState.selected) ? 
-      GestureDetector(
-        onTapUp: (details) async{
-          await widget.onSeatStateChanged(widget.model.seat, details);
-          setState(() { 
-          });
-        },
-        child: buildSeat()
-      ) : buildSeat();
+    return RepaintBoundary(
+        child: (widget.model.seat.status == SeatState.available ||
+                widget.model.seat.status == SeatState.selected)
+            ? GestureDetector(
+                onTapUp: (details) async {
+                  await widget.onSeatStateChanged(widget.model.seat, details);
+                  setState(() {});
+                },
+                child: buildSeat())
+            : buildSeat());
   }
 
-  Widget buildSeat(){
+  Widget buildSeat() {
+    print("BUILDING SEAT WIDGET3 " + DateTime.now().toString());
     return widget.model.seat != SeatState.empty
-      ? Stack(
-        alignment: Alignment.center,
-        children:[
-          SvgPicture.asset(
-            _getSvgPath(widget.model.seat.status),
+        ? Stack(alignment: Alignment.center, children: [
+            SvgPicture.asset(
+              _getSvgPath(widget.model.seat.status),
+              height: widget.model.seatSvgSize.toDouble(),
+              width: widget.model.seatSvgSize.toDouble(),
+              fit: BoxFit.cover,
+            ),
+            Text(widget.model.seat.seat.seatNumber,
+                style: widget.textStyle ?? TextStyle(fontFamily: 'monospace'))
+          ])
+        : SizedBox(
             height: widget.model.seatSvgSize.toDouble(),
             width: widget.model.seatSvgSize.toDouble(),
-            fit: BoxFit.cover,
-          ),
-          Text(
-            widget.model.seat.seat.seatNumber,
-            style: widget.textStyle ?? TextStyle(fontFamily: 'monospace')
-          )
-        ] 
-      )
-      : SizedBox(
-          height: widget.model.seatSvgSize.toDouble(),
-          width: widget.model.seatSvgSize.toDouble(),
-      );
+          );
   }
 
   String _getSvgPath(SeatState state) {
